a977ca89fd6e0795e4b78d2992f1b44194a60e22
IK <ik@develup.pro>
2017-04-25 18:47:32 +0300

    upd

--- compiled/server.js
+++ compiled/server.js
@@ -12,12 +12,28 @@ let path = "./data/food.xlsx";
 let parsedFile = new ExcelParser_1.ExcelParser(path);
 //foodBot.botInstance.sendMessage(32317725,"dsadasdsadas");
 //slava chat ID 42346292
+foodBot.botInstance.onText(/\/menu/, function onLoveText(msg) {
+    let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
+    const opts = {
+        reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                [{
+                        text: 'Понедельник',
+                        callback_data: '/monday'
+                    }, '/tuesday'],
+                ['/wednesday', '/thursday'],
+                ['/friday']
+            ]
+        })
+    };
+    foodBot.botInstance.sendMessage(msg.chat.id, 'На какой день еду показать?', opts);
+    //foodBot.botInstance.sendMessage(msg.from.id, 'Original Text', opts);
+});
 foodBot.botInstance.on('message', (msg) => {
     const chatId = msg.chat.id;
-    console.log(chatId);
     let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
     if (weeks.indexOf(msg.text) >= 0) {
-        //console.log(parsedFile.getMenuByWeekDay(msg.text));
         let message = parsedFile.getMenuByWeekDay(msg.text.replace('/', ''));
         message = message.join('\n');
         message = message.replace(/&quot;/g, '"');
@@ -26,8 +42,29 @@ foodBot.botInstance.on('message', (msg) => {
     else
         foodBot.botInstance.sendMessage(chatId, "тупо пересылаю что мне шлют '" + msg.text + "'");
 });
+foodBot.botInstance.on('callback_query', function onCallbackQuery(callbackQuery) {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    const opts = {
+        chat_id: msg.chat.id,
+        message_id: msg.message_id,
+    };
+    let text;
+    /*if (action === 'edit') {
+      text = 'Edited Text';
+    }*/
+    console.log(action);
+    let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
+    if (weeks.indexOf(action) >= 0) {
+        let message = parsedFile.getMenuByWeekDay(action.replace('/', ''));
+        message = message.join('\n');
+        message = message.replace(/&quot;/g, '"');
+        foodBot.botInstance.sendMessage(opts.chat_id, message);
+    }
+    // else foodBot.botInstance.sendMessage(chatId, "тупо пересылаю что мне шлют '" + msg.text + "'");
+    //foodBot.botInstance.editMessageText(text, opts);
+});
 app.listen(port, () => {
-    // Success callback
     console.log(`Listening at http://localhost:${port}/`);
 });
 //# sourceMappingURL=server.js.map
--- source/server.ts
+++ source/server.ts
@@ -15,22 +15,68 @@ let parsedFile = new ExcelParser(path);
 
 //foodBot.botInstance.sendMessage(32317725,"dsadasdsadas");
 //slava chat ID 42346292
-foodBot.botInstance.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  console.log (chatId);
-  let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
 
-  if (weeks.indexOf(msg.text) >= 0) {
-    //console.log(parsedFile.getMenuByWeekDay(msg.text));
-    let message : any = parsedFile.getMenuByWeekDay(msg.text.replace('/',''));
-    message = message.join('\n');
-    message = message.replace(/&quot;/g,'"');
-    foodBot.botInstance.sendMessage(chatId, message);
-    
-  } else  foodBot.botInstance.sendMessage(chatId, "тупо пересылаю что мне шлют '" + msg.text + "'");
+foodBot.botInstance.onText(/\/menu/, function onLoveText(msg) {
+  let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{
+            text: 'Понедельник',
+            callback_data: '/monday'
+          },'/tuesday'], 
+        ['/wednesday','/thursday'], 
+        ['/friday']
+      ]
+    })
+  };
+  foodBot.botInstance.sendMessage(msg.chat.id, 'На какой день еду показать?', opts);
+  //foodBot.botInstance.sendMessage(msg.from.id, 'Original Text', opts);
 });
 
+
+foodBot.botInstance.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  
+  let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
+  if (weeks.indexOf(msg.text) >= 0) {    
+    let message: any = parsedFile.getMenuByWeekDay(msg.text.replace('/', ''));
+    message = message.join('\n');
+    message = message.replace(/&quot;/g, '"');
+    foodBot.botInstance.sendMessage(chatId, message);
+  } else foodBot.botInstance.sendMessage(chatId, "тупо пересылаю что мне шлют '" + msg.text + "'");
+
+});
+
+foodBot.botInstance.on('callback_query', function onCallbackQuery(callbackQuery) {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+  };
+  let text;
+
+  /*if (action === 'edit') {
+    text = 'Edited Text';
+  }*/
+  console.log (action);
+
+  let weeks = ['/monday', '/tuesday', '/wednesday', '/thursday', '/friday'];
+  if (weeks.indexOf(action) >= 0) {
+    let message: any = parsedFile.getMenuByWeekDay(action.replace('/', ''));
+    message = message.join('\n');
+    message = message.replace(/&quot;/g, '"');
+    foodBot.botInstance.sendMessage(opts.chat_id, message);
+  }
+  // else foodBot.botInstance.sendMessage(chatId, "тупо пересылаю что мне шлют '" + msg.text + "'");
+
+  //foodBot.botInstance.editMessageText(text, opts);
+});
+
+
+
 app.listen(port, () => {
-  // Success callback
   console.log(`Listening at http://localhost:${port}/`);
 });
